7b29ad1846001e8124500d75df3e98b641abe19b
Jarryd <jarryd@ajency.in>
2013-10-22 16:17:37 +0530

    New Layout Mode Switch

--- js/global.js
+++ js/global.js
@@ -3,7 +3,7 @@
  * All required jquery plugins + other libraries are looaded througth 
  * this file.
  */
-define(['underscore', 'jquery', 'backbone', 'moment', 'numerals', 'bootstrap', 'text', 'jqueryui', 'string', 'cookie', 'bootstrapselect', 'bootstrapswitch'], 
+define(['underscore', 'jquery', 'backbone', 'moment', 'numerals', 'bootstrap', 'text', 'jqueryui', 'string', 'cookie', 'bootstrapselect'], 
 		function(_ , $ , Backbone, moment, numerals){
 
 			var global = {};
