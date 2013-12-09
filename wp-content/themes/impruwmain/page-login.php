f4a2e8b443f0910307c1178a138e4400a0b70e16
Parag Redkar <parag@ajency.in>
2013-12-09 09:25:15 +0530

    login page

--- wp-content/themes/impruwmain/page-login.php
+++ wp-content/themes/impruwmain/page-login.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Register
+ * Template Name: Login
  */
 //echo ABSPATH;
 //include(ABSPATH."wp-content\\themes\impruwmain\User\user_management.php");
@@ -40,11 +40,17 @@
 </head>
 
 <body <?php //body_class(); ?>>
-    <div class="aj-imp-container container">
+
+
+
+
+
+
+<div class="aj-imp-container container">
 		<div class="aj-imp-login-form">
 			<div class="row">
 				<div class="col-sm-12 aj-imp-login-header">
-					<h1><?php echo __('Login','impruwmain'); ?> <span><?php echo __('Impruw','impruwmain');?></span></h1>
+					<h1><?php echo __('Login','impruwmain'); ?>  <span><?php echo __('Impruw','impruwmain');?></span></h1>
 					<p class="desc">
 						<?php echo __('Lorem Ipsum is simply dummy text of the printing and typesetting industry.','impruwmain'); ?>
 					</p>
@@ -52,35 +58,40 @@
 			</div>
 			<div class="row">
 				<div class="col-sm-12 aj-imp-login">
-					<form class="form-horizontal clearfix"  parsley-validate  >
+					<form class="form-horizontal clearfix" id="frm_login" name="frm_login" >
+					<?php   $ajax_nonce = wp_create_nonce( "frm_login" ); 
+						echo "<script> var ajax_nonce ='".$ajax_nonce."' </script>";
+						?>
+						<span id="login_status"></span>
 						<div class="row">
 							<label for="inputEmail" class="col-sm-3 control-label"><?php echo __('Email','impruwmain'); ?></label>
-							<div class="col-sm-7">
+							<div class="col-sm-7 col-sm-offset-3">
 								<div class="form-group">
-									<input type="email" class="form-control" id="inputEmail" placeholder="Email"  required   parsley-trigger="blur" parsley-validation-minlength="0"    parsley-required-message="Please Enter Email">
-								</div>
+									<input type="email" class="form-control  parsley-validated parsley-error" id="inputEmail"  name="inputEmail" placeholder="Email"    parsley-required="true"    parsley-trigger="blur"    parsley-validation-minlength="0"   parsley-type="email"   parsley-required-message="<?php echo __('Please Enter Email','impruwmain'); ?>" />
+								</div> 
 							</div>
 						</div>
 						<div class="row">
-							<label for="inputPass" class="col-sm-3 control-label"><?php echo __('Password','impruwmain'); ?></label>
-							<div class="col-sm-7">
+							<label for="inputPass" class="col-sm-3 control-label"><?php echo __('Password','impruwmain');?></label>
+							<div class="col-sm-7 col-sm-offset-3">
 								<div class="form-group">
-									<input type="password" class="form-control" id="inputPass" placeholder="Password"   required   parsley-trigger="blur" parsley-validation-minlength="0"    parsley-required-message="Please Enter Password">
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-sm-offset-3 col-sm-7">
-								<div class="form-group">
-									<button type="button" id="btn_login" class="btn btn-wide aj-imp-submit"><?php echo __('Login','impruwmain'); ?></button>
-									<a href="#" class="aj-imp-log-forgot"><?php echo __('Forgot your password?','impruwmain'); ?></a>
+									<input type="password" class="form-control  parsley-validated parsley-error" id="inputPass"  name="inputPass" placeholder="Password"    parsley-required="true"     parsley-trigger="blur" parsley-validation-minlength="0"    parsley-required-message="<?php echo __('Please Enter Password','impruwmain');?>" />
 								</div>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-sm-offset-3 col-sm-7">
 								<div class="form-group">
-									<?php echo __('Dont have an account','impruwmain?'); ?> <a href="register.html"><?php echo __('Sign Up','impruwmain'); ?></a>
+									<button type="button" id="btn_login" name="btn_login" class="btn btn-wide aj-imp-submit"><?php echo __('Login','impruwmain'); ?></button>
+									<img id="login_loader" src="<?php echo site_url()."/wp-content/themes/impruwmain/images/loader.gif"; ?>" width="38" height="30"  style="display:none;"/>
+									<a href="#" class="aj-imp-log-forgot"><?php echo __('Forgot your password?','impruwmain');?></a>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-offset-3 col-sm-7">
+								<div class="form-group">
+									<?php echo __('Dont have an account?','impruwmain');?> <a href="<?php  echo site_url()."/register"; ?>"><?php echo __('Sign Up','impruwmain');?></a>
 								</div>
 							</div>
 						</div>
@@ -89,6 +100,16 @@
 			</div>
 		</div>
 	</div>
+
+
+
+
+
+
+
+
+
+    
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <!--     <script src="js/jquery-2.0.3.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -- >
