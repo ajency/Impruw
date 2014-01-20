eafc6a98227803396a0d040c8c2c1b52488880f2
Jarryd <jarryd@ajency.in>
2014-01-20 18:17:04 +0530

    Default Page Template for Impruw Main Theme

---
+++ wp-content/themes/impruwmain/page.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Default
+ */
+
+get_header();
+
+if ( have_posts() ) : while ( have_posts() ) : the_post();
+?>
+
+<div class="aj-imp-register-form">
+	<div class="row">
+		<div class="col-sm-12 aj-imp-register-header">
+			<h1><?php the_title(); ?></h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<?php 
+				// Get Post Content
+				the_content();
+			?>
+		</div>
+	</div>
+</div>
+		
+<?php
+endwhile; endif;
+
+ get_footer(); ?>    
