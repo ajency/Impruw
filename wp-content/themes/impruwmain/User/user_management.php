4f9a2068f9e27405ab47ae6e4a55f80e295a68a3
Suraj Air <suraj@ajency.in>
2014-01-21 13:25:59 +0530

    activate polugin

--- wp-content/themes/impruwmain/User/user_management.php
+++ wp-content/themes/impruwmain/User/user_management.php
@@ -274,7 +274,7 @@ function toggle_plugin( $blog_id ) {
         $_REQUEST['action'] = 'activate';
         $_REQUEST['_wpnonce'] = wp_create_nonce();
         activate_plugin( $my_plugin );
-        activate_plugin( $my_plugin );
+        //activate_plugin( $my_plugin );
         //icl_sitepress_activate();
     }
     /*$my_plugin = 'wpml-string-translation/plugin.php';
