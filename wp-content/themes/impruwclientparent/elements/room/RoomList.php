a5196c524b4e678605cc9f0d81928bc395c2c0b4
Suraj Air <suraj@ajency.in>
2014-01-21 15:35:26 +0530

    rooms list element added

--- wp-content/themes/impruwclientparent/elements/room/RoomList.php
+++ wp-content/themes/impruwclientparent/elements/room/RoomList.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This class is responsible for all actions/functions related to 
- * RoomGallery
+ * Room List Element
  *
  * @category   element
  * @package    Impruw
@@ -15,15 +15,13 @@
  * @deprecated NA
  */
 
-require_once PARENTTHEMEPATH . 'elements/SliderElement.php';
-
-class RoomGallery extends SliderElement {
+class RoomList extends Element {
     
     /**
      * The default type property for element
      * @var String 
      */
-    var $type       = 'roomgallery';
+    var $type       = 'roomlist';
     
     /**
      * Set draggable property of element.
@@ -37,18 +35,37 @@ class RoomGallery extends SliderElement {
      * Empty string by default
      * @var String 
      */
-    var $class_name  = 'room-gallery';
+    var $class_name  = 'room-list';
     
     
     
     /**
-     * The config to create a row element
+     * The config to create a room list element
      * @param array $config
      */
     function __construct($config, $post_id = 0) {
         
         parent::__construct($config);
 
+        $this->markup  = $this->generate_markup();
+
+    }
+
+    /**
+     * [generate_markup description]
+     * @return [type] [description]
+     */
+    function generate_markup(){
+        
+        $html = '';
+
+        ob_start(); 
+        ?>
+        <h1>place html here</h1>
+        <?php
+        $html .= ob_get_clean();
+
+        return $html;
     }
     
 }
