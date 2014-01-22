73800083e4af9402a473db3c7b66570ae8029446
Suraj Air <suraj@ajency.in>
2014-01-22 15:07:39 +0530

    Room gallery images

--- wp-content/themes/impruwclientparent/elements/room/RoomGallery.php
+++ wp-content/themes/impruwclientparent/elements/room/RoomGallery.php
@@ -85,9 +85,8 @@ class RoomGallery extends SliderElement {
 
         $ids = implode(',', $this->data_source['image-ids']);
 
-        //$html = do_shortcode("[gallery ids='95,93,92,91,94']");
-
-        $html = do_shortcode('[gallery ids="96,92,93"]');
+        $html = do_shortcode("[gallery ids='$ids']");
+        //$html = do_shortcode('[gallery ids="96,92,93"]');
 
         remove_filter('wp_get_attachment_link', 'add_rel_attribute', 100, 6);
 
