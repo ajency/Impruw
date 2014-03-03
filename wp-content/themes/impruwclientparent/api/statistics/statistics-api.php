ed2bd4417a74ee81f694db15f3171dd96459d733
mahima <mahima@ajency.in>
2014-03-03 12:10:26 +0530

    updated analytics data fetching php file

--- wp-content/themes/impruwclientparent/api/statistics/statistics-api.php
+++ wp-content/themes/impruwclientparent/api/statistics/statistics-api.php
@@ -4,12 +4,64 @@
  * All of the stattistics related code goes here
  *///initial google setup
 
+// api dependencies
+require_once('google-api-php-client/src/Google_Client.php');
+require_once('google-api-php-client/src/contrib/Google_AnalyticsService.php');
 
+function get_data($start_date,$end_date,$profile_id,$metrics,$optParams){
 
+	$client= google_analytics_object();
 
+	// create service and get data
+	$service = new Google_AnalyticsService($client);
+	
+	//var_dump($service->management_accounts->listManagementAccounts());
+    try{
+		$response = $service->data_ga->get(
+		         'ga:'.$profile_id, // profile id
+		         $start_date, // start date
+		         $end_date, // end date
+		         $metrics,
+		         $optParams
+		        );
+		$response2=  array('status'=>'1','data3'=>$response);
 
+		return $response2;
+	}
+	catch(Exception $e) {
+		$error_res = array('status'=>'0','error_code'=>$e->getCode(),'error_msg'=>$e->getMessage());
+		return $error_res;
+    	
+	}
+}
 
+function google_analytics_object(){
+	
+	// create client object and set app name
+	$client = new Google_Client();
+	$client->setApplicationName('ServiceProject'); // name of your app
 
+	// set session
+	session_start();
+	if (isset($_SESSION['token'])) {
+	 $client->setAccessToken($_SESSION['token']);
+	}
+	
+	// set assertion credentials
+	$client->setAssertionCredentials(
+	    new Google_AssertionCredentials(
+	        '53154612183-dadqb19o205cv525ibqt26e8ov7pk1rt@developer.gserviceaccount.com', // email you added to GA
+	        array('https://www.googleapis.com/auth/analytics.readonly'),
+	       file_get_contents("4d754bad091f541088b6080ad9868be405a26c61-privatekey.p12") // keyfile you downloaded
+	    )
+	);
+
+	// other settings
+	$client->setClientId('53154612183-dadqb19o205cv525ibqt26e8ov7pk1rt.apps.googleusercontent.com'); // from API console
+	$client->setAccessType('offline_access'); // this may be unnecessary?
+
+	return $client;
+}
 
 
 
@@ -19,14 +71,83 @@
 function get_unique_page_views(){
 
 	//call the google API method here to fetch the data
-	
+	$start_date="2014-02-01"; //$_POST['start_date'];
+	$end_date="2014-02-20"; //$_POST['end-date'];
+	$profile_id= "81856773"; //$_POST['ids'];
+	$metrics= 'ga:visits,ga:visitors,ga:newVisits,ga:pageviews,ga:pageviewsPerVisit,ga:bounces'; //$_POST['metrics'];
+	$dimensions = 'ga:date';
+	$optParams = array('dimensions' => $dimensions);
+
+	$metrics_name= explode(',',$metrics);
 
 	// format the returned data according to required model/collection. (this u can ignore for now)
-	$data = '';//formated data
+	$data = get_data($start_date,$end_date,$profile_id,$metrics,$optParams);//formated data
+	$arr= array();
+    //echo '<pre>';
+    //print_r($data);
+    
+    if($data['status'] != 0){
+    	$arr = make_array($data['data3']['rows'],$metrics_name);	
+	}
+    else{
+	 	$arr =  array('Error_code' => $data['error_code'],'Error_Msg' => $data['error_msg']);
+	} 
 
-	wp_send_json(array('code' => 'OK', 'data' => $data));
+	//echo '<pre>';
+   // print_r($arr);
+
+   wp_send_json(array('response' => $arr));
 }
+
+function make_array($array_val,$metrics_name){
+
+	$metr= $metrics_name;
+
+    // looping through the rows array
+	foreach ($array_val as $key => $values) {
+		$date= $array_val[$key][0];
+		$date2= date("m/d/y", strtotime($date));
+		$condate = strtotime($date2) * 1000;
+		$arr[] = array('date' => $condate);
+		$metric_name_val = make_metric_arr($metr,$values);
+		
+	    // looping through the metric array and merging with the arr[] array
+	    foreach ($metric_name_val as $key_val => $arr_val) {
+	    	foreach ($arr_val as $key_val2 => $arr_val2) {
+	    		$arr[$key][$key_val2] = $arr_val2;
+	    	}
+	    }
+
+	}
+	return $arr;
+}
+
+function make_metric_arr($metric_name,$values){
+
+    // setting the indexes of the array to 0
+    unset($values[0]);
+    $values2 = array_values($values);	
+
+	// loop to prepare the metrics array with their value
+	for($i=0; $i<count($metric_name); $i++){
+
+		$metric_name_val[] = array($metric_name[$i] => $values2[$i] );
+
+	}
+	
+	return $metric_name_val;
+		
+}
+
 add_action('wp_ajax_get-unique-page-views', 'get_unique_page_views');
+
+
+
+
+
+
+
+
 
 // //defining ajax
 // function get_unique_page_views(){
