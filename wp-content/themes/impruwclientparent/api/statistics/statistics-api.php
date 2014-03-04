174722bca76337719648fe1b86a9fa02ea442286
mahima <mahima@ajency.in>
2014-03-04 12:42:17 +0530

    ga wordpress plugin

--- wp-content/themes/impruwclientparent/api/statistics/statistics-api.php
+++ wp-content/themes/impruwclientparent/api/statistics/statistics-api.php
@@ -15,7 +15,6 @@ function get_data($start_date,$end_date,$profile_id,$metrics,$optParams){
 	// create service and get data
 	$service = new Google_AnalyticsService($client);
 	
-	//var_dump($service->management_accounts->listManagementAccounts());
     try{
 		$response = $service->data_ga->get(
 		         'ga:'.$profile_id, // profile id
@@ -50,15 +49,15 @@ function google_analytics_object(){
 	// set assertion credentials
 	$client->setAssertionCredentials(
 	    new Google_AssertionCredentials(
-	        '53154612183-dadqb19o205cv525ibqt26e8ov7pk1rt@developer.gserviceaccount.com', // email you added to GA
+	        GA_EMAIL, // email you added to GA
 	        array('https://www.googleapis.com/auth/analytics.readonly'),
-	       file_get_contents("4d754bad091f541088b6080ad9868be405a26c61-privatekey.p12") // keyfile you downloaded
+	       file_get_contents(GA_KEY_FILE) // keyfile you downloaded
 	    )
 	);
 
 	// other settings
-	$client->setClientId('53154612183-dadqb19o205cv525ibqt26e8ov7pk1rt.apps.googleusercontent.com'); // from API console
-	$client->setAccessType('offline_access'); // this may be unnecessary?
+	$client->setClientId(GA_CLIENT_ID); // from API console
+	$client->setAccessType('offline_access'); 
 
 	return $client;
 }
@@ -68,7 +67,7 @@ function google_analytics_object(){
 //defining ajax
 //// url to test this function :
 ///http://localhost/impruw/wp-admin/admin-ajax.php?action=get-unique-page-views
-function get_unique_page_views(){
+function get_analytics_data(){
 
 	//call the google API method here to fetch the data
 	$start_date="2014-02-01"; //$_POST['start_date'];
@@ -93,10 +92,41 @@ function get_unique_page_views(){
 	 	$arr =  array('Error_code' => $data['error_code'],'Error_Msg' => $data['error_msg']);
 	} 
 
-	//echo '<pre>';
-   // print_r($arr);
+  // echo '<pre>';
+  // print_r($arr);
 
    wp_send_json(array('response' => $arr));
+}
+
+/**
+ * Define a function to get data from ga
+ * this function will get all filters/9dimensions/ metrix and query te ga Api.
+ * andle no response and actual response
+ * Finally send the response are json data 
+ */ 
+function abc(){
+
+	//get the start date
+	$sdsdas = sdsadas;
+
+
+	// get end date
+		// 
+
+	///
+	//
+
+	//get the date querying google API, 
+
+	//check for repons status
+	// if proper 
+
+
+	//else
+
+
+
+
 }
 
 function make_array($array_val,$metrics_name){
@@ -114,7 +144,7 @@ function make_array($array_val,$metrics_name){
 	    // looping through the metric array and merging with the arr[] array
 	    foreach ($metric_name_val as $key_val => $arr_val) {
 	    	foreach ($arr_val as $key_val2 => $arr_val2) {
-	    		$arr[$key][$key_val2] = $arr_val2;
+	    		$arr[$key][$key_val2] = (float)$arr_val2;
 	    	}
 	    }
 
@@ -122,6 +152,15 @@ function make_array($array_val,$metrics_name){
 	return $arr;
 }
 
+/**
+ *
+ * 
+ *
+ * @param string $format Format of the date to return.
+ * @param string $date Date string to convert.
+ * @param bool $translate Whether the return date should be translated. Default is true.
+ * @return string|int Formatted date string, or Unix timestamp.
+ */
 function make_metric_arr($metric_name,$values){
 
     // setting the indexes of the array to 0
@@ -139,7 +178,7 @@ function make_metric_arr($metric_name,$values){
 		
 }
 
-add_action('wp_ajax_get-unique-page-views', 'get_unique_page_views');
+add_action('wp_ajax_get_analytics_data', 'get_analytics_data');
 
 
 
