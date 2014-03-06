7a8170374c5d5b1a52a6fde1615c3b9ee902102e
mahima <mahima@ajency.in>
2014-03-06 17:05:02 +0530

    add new slide to the revolution slider

--- wp-content/themes/impruwclientparent/modules/slider/ajax.php
+++ wp-content/themes/impruwclientparent/modules/slider/ajax.php
@@ -68,15 +68,51 @@ function delete_slider(){
 }
 add_action('wp_ajax_delete-slider', 'delete_slider'); 
 
-
-/********** Slides ******************/
+/**
+* Fetching the details of all the slides 
+* for a specified slider. Slider ID should be passed
+* to the function and it returns a json array containing 
+* all the details of the slides present in the specified 
+* slider.
+*
+* @param string $slider_id is the ID of the slider
+* @param function fetch_slide() returns a json array containing 
+* the slider name,transistions,slot amount,rotation,transition duration,
+* delay,enable link and thumbnail of each slide in the slider specified.
+*  
+*/
 
 function fetch_slides(){
     
+    // get the slider ID from the call
     $slider_id = $_GET['slider_id'];
     
+    // call the get_slides function in the functions.php file along with the slider ID.
+	// The function will return an array containing the slides data 
     $slides_arr = get_slides($slider_id);
     
     wp_send_json(array('code' => 'OK', 'data' => $slides_arr));
 }
 add_action('wp_ajax_fetch-slides','fetch_slides');
+
+
+function create_slide(){
+
+	//$data = $_POST;
+	$data = array(
+		          'image_id' => 100,
+		          'image'=> 'http://localhost/impruw/childsite/wp-content/uploads/sites/81/2014/03/images-4.jpg',
+		          'title' =>'Slide',
+	              'background_type' => 'image'
+		          );
+
+	//unset($data['action']);
+    //unset($data['wait']);
+
+	$id = create_new_slide($data,$slider_id,$slide_order);
+
+	wp_send_json(array('code' => 'OK', 'data' => array ('id' => $id)));
+	
+	
+}
+add_action('wp_ajax_create-slide', 'create_slide'); 
