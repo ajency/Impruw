26c0e9b956a61d5a73e782147c1b6a5deeddc3a5
Jarryd <jarryd@ajency.in>
2014-03-18 14:21:36 +0530

    Less Files Structure for Parent Theme

--- wp-content/themes/impruwclientparent/new-builder.php
+++ wp-content/themes/impruwclientparent/new-builder.php
@@ -28,6 +28,7 @@
     <link href="<?php echo get_parent_template_directory_uri(); ?>/builder/css/main.css"    rel="stylesheet" media="screen"/>
     <link href="<?php echo get_parent_template_directory_uri(); ?>/builder/css/builder.css" rel="stylesheet" media="screen"/>
     <link href="<?php echo get_parent_template_directory_uri(); ?>/builder/css/custom.css"  rel="stylesheet" media="screen"/>
+    <link href="<?php echo get_parent_template_directory_uri(); ?>/css/style.css" rel="stylesheet" media="screen"/>
     <link href="<?php echo get_template_directory_uri(); ?>/css/style.css" rel="stylesheet" media="screen"/>
     <link href="<?php echo get_template_directory_uri(); ?>/css/slimmenu.min.css" rel="stylesheet" media="screen"/>
 </head>
