b2dca5257f3c4db7bec32c1f519542722ed09659
Suraj Air <suraj@ajency.in>
2014-03-26 15:56:39 +0530

    Image with text element added

---
+++ wp-content/themes/impruwclientparent/elements/ImageWithText.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * This class is responsible for all actions/functions related to 
+ * BuilderRowColumn Element
+ *
+ * @category   layout
+ * @package    Impruw
+ * @author     Suraj Air<suraj@ajency.in>
+ * @author     Suraj Air<suraj@ajency.in>
+ * @date       28th Nov 13
+ * @copyright  2013 Ajency.in
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    Release: 0.1
+ * @since      Class available since Release 0.1
+ * @deprecated NA
+ */
+
+class ImageWithText extends Element {
+    
+    /**
+     * The default type property for element
+     * @var String 
+     */
+    var $type       = 'image';
+    
+    /**
+     * Set draggable property of element.
+     * All elements are draggable by defaults
+     * @var boolean 
+     */
+    var $tag_name        = 'div';
+    
+    /**
+     * The default classname property for element.
+     * Empty string by default
+     * @var String 
+     */
+    var $class_name  = 'imagewithtext';
+    
+    
+    
+    /**
+     * The config to create a row element
+     * @param array $config
+     */
+    function __construct($element) {
+        
+        parent::__construct($element);
+        
+        $this->image_id = $element['image_id'];
+        $this->size 	= $element['size'];
+        $this->align    = $element['align'];
+        $this->content 	= $element['content'];
+        $this->style 	= sanitize_title($element['content']);
+        $this->markup   = $this->generate_markup();
+        
+    }
+    
+    /**
+     * Create the basic markup for an element
+     * @uses className and tagName properties of element
+     * @return String basic markup
+     */
+    function generate_markup(){
+    	
+    	global $me;
+    	
+    	$template = '<div class={{class_name}}>
+	    				{{#image}}
+							<img src="{{imageurl}}" alt="{{title}}" class="{{alignclass}} img-responsive"/>
+						{{/image}}
+						{{#placeholder}}
+							<div class="image-placeholder"><span class="bicon icon-uniF10E"></span>Image</div>
+						{{/placeholder}}
+						<p class="editor">{{content}}</p>
+						<div class="clearfix"></div>
+    				 </div>';
+    	
+    	$data = array();
+    	
+    	if($this->image_id > 0){
+    		$data['image'] = true;
+    		$image = '';
+    		$data['imageurl'] = $image->sizes['thumbnail']->url;
+    		$data['title'] = $image->title;
+    		$data['alignclass'] = $this->align === 'right' ? 'pull-left' : 'pull-left';	
+    	}
+    	else{
+    		$data['placeholder'] = true;
+    	}
+    	
+    	$data['content'] = $this->content;
+    	$data['class_name'] = $this->style;
+    	
+    	$html = $me->render($template, $data);
+    	
+    	return $html;
+    }
+    
+}
