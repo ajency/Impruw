802fc1922729fc0a0227bb593b1a579fa248c755
Jarryd <jarryd@ajency.in>
2014-03-26 15:28:29 +0530

    Room Summary Element Markup and Styles

--- wp-content/themes/impruwclientparent/functions.php
+++ wp-content/themes/impruwclientparent/functions.php
@@ -3255,11 +3255,11 @@ $base_element_templates = array(
 	'RoomSummary' => array(
 				array(
 					'name' => 'Room Summary Default',
-					'template' => '<div class="room-img"><img src=""></div><div class="room-title">Room Title</div><div class="room-excerpt">Lorem Ipsum Dolor Sit Amet</div><div class="room-actions"><div class="price">$199<small>/night</small></div><button class="btn btn-room">View Details</button></div>'
+					'template' => '<div class="room-img"><img src="" class="img-responsive"></div><div class="room-title">Room Title</div><div class="room-excerpt">Lorem Ipsum Dolor Sit Amet</div><div class="room-actions"><div class="price">$199<small>/night</small></div><button class="btn btn-room">View Details</button></div>'
 				),
 				array(
 					'name' => 'Room Summary New',
-					'template' => '<div class="room-img"><img src=""></div><div class="room-title">Room Title</div><div class="room-excerpt">Lorem Ipsum Dolor Sit Amet</div><div class="room-actions"><div class="price">$199<small>/night</small></div><button class="btn btn-room">View Details</button></div>'
+					'template' => '<div class="room-img"><img src="" class="img-responsive"></div><div class="room-title">Room Title</div><div class="room-excerpt">Lorem Ipsum Dolor Sit Amet</div><div class="room-actions"><div class="price">$199<small>/night</small></div><button class="btn btn-room">View Details</button></div>'
 				)
 			)
 );
