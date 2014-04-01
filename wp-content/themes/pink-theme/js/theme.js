b57b56a0fc738f2d2ca9c5e031549b0e78d573b1
Jarryd <jarryd@ajency.in>
2014-04-01 18:26:57 +0530

    Pink Theme Fix and Dashboard Markup Fix

--- wp-content/themes/pink-theme/js/theme.js
+++ wp-content/themes/pink-theme/js/theme.js
@@ -5,6 +5,7 @@ jQuery(window).resize(function() {
 	var f = jQuery('.site-footer').height();
 	var h = jQuery(document).height() - (f);
 	jQuery('.long-bar > .column').height(h);
+	jQuery('.site-page').height(h);
 });
 jQuery(window).load(function() { 
 	var f = jQuery('.site-footer').height();
