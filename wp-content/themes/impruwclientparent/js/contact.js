b15e6dbf79b42cf75208ae17d263ef2599cc77e3
Mahima Manu <mahima@ajency.in>
2014-04-10 17:25:16 +0530

    calender

--- wp-content/themes/impruwclientparent/js/contact.js
+++ wp-content/themes/impruwclientparent/js/contact.js
@@ -278,7 +278,7 @@ jQuery(document).ready(function($) {
         var html = '<div class="item active">\n\
                         <div class="room-booking-plan">\n\
                             <h5>\n\
-                                Selected date does not exsists in the\n\
+                                Selected date does not exsists in any of the\n\
                                 available dateranges\n\
                             </h5>\n\
                         </div>\n\
@@ -380,15 +380,15 @@ jQuery(document).ready(function($) {
 
             }
             else {
-                var plans_name = getPlans(TARIFF[i].plan_id);
+               /* var plans_name = getPlans(TARIFF[i].plan_id);
                 html += '<div class="item">\n\
                                 <div class="room-booking-plan">' + plans_name +
                         '</div>\n\
                                 <div class="booking-detail">\n\
                                     No tariff data available for selected date\n\
                                 </div>\n\
-                                </div>';
-
+                                </div>';*/
+                
 
             }
         }
