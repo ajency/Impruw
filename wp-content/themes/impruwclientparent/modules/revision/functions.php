f08ae6025a0a645013ae949877863f757de2273e
Suraj Air <suraj@ajency.in>
2014-05-12 12:55:13 +0530

    refactored ajax

--- wp-content/themes/impruwclientparent/modules/revision/functions.php
+++ wp-content/themes/impruwclientparent/modules/revision/functions.php
@@ -26,7 +26,7 @@
      *
      * @return int
      */
-    function get_last_revision_id($page_id = 0)
+    function  get_last_revision_id($page_id = 0)
     {
 
         $revisions = wp_get_post_revisions($page_id);
@@ -132,7 +132,7 @@
         global $wpdb;
 
         // cannot use get_option here as it returns only one record.
-        // we will get the record which will have the highedt option_id value
+        // we will get the record which will have the highest option_id value
         $query = $wpdb->prepare("SELECT option_id FROM {$wpdb->options}
                             WHERE option_name=%s 
                             ORDER BY option_id DESC LIMIT 1", "theme-$section");
@@ -227,7 +227,6 @@
      */
     function pluck_meta_ids($element, &$meta_ids)
     {
-
 
         if ($element['element'] === "Row") {
             foreach ($element['elements'] as $column) {
