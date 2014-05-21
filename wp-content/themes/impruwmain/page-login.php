e6432c210f58efbb89824ce6af9845f7fa9e1326
Jarryd <jarryd@ajency.in>
2014-05-21 13:26:17 +0530

    Login Forgot Password Form + Styles and Interaction

--- wp-content/themes/impruwmain/page-login.php
+++ wp-content/themes/impruwmain/page-login.php
@@ -78,6 +78,57 @@
                 </form>
             </div>
         </div>
+
+        <!-- Forgot Password Form -->
+        <div class="row">
+            <div class="col-sm-12 aj-imp-forgot hidden">
+                <form class="form-horizontal clearfix" id="" name="">
+                    
+                    <div class="form-group">
+                        <label for="inputEmail"
+                               class="col-sm-3 control-label"><?php echo __('Email', 'impruwmain'); ?></label>
+
+                        <div class="col-sm-6 col-sm-offset-3">
+
+                            <input type="email" class="form-control  parsley-validated parsley-error" id="inputEmail"
+                                   name="inputEmail" placeholder="Email ID you signed up with" parsley-required="true"
+                                   parsley-trigger="blur" parsley-validation-minlength="0" parsley-type="email"
+                                   parsley-required-message="<?php echo __('A valid email address is required to sign in', 'impruwmain'); ?>"/>
+
+                            <div class="p-messages"></div>
+
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col-sm-offset-3 col-sm-7">
+                            <button type="button" id="" name=""
+                                    class="btn btn-wide aj-imp-submit"><?php echo __('Reset Password', 'impruwmain'); ?></button>
+                            <img id="login_loader"
+                                 src="<?php echo site_url() . "/wp-content/themes/impruwmain/images/loader.gif"; ?>"
+                                 width="38" height="30" style="display:none;"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-offset-3 col-sm-6" style="margin-top: 1em;">
+                            <a href="#" class="aj-imp-sign-back"><?php echo __('&laquo; Sign In', 'impruwmain'); ?></a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
+    <script type="text/javascript">
+        jQuery(document).ready(function(){
+            jQuery('.aj-imp-log-forgot').click(function(){
+                jQuery('.aj-imp-login').addClass('hidden');
+                jQuery('.aj-imp-forgot').removeClass('hidden');
+            });
+            jQuery('.aj-imp-sign-back').click(function(){
+                jQuery('.aj-imp-login').removeClass('hidden');
+                jQuery('.aj-imp-forgot').addClass('hidden');
+            });
+        });
+    </script>
 <?php
     get_footer();
