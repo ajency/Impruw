d1d8db5f1ace9ebfe69917b57bf3f0573ea03144
Jarryd <jarryd@ajency.in>
2014-05-22 19:21:00 +0530

    Impruw Home Page Template and Styles

---
+++ wp-content/themes/impruwmain/page-homepage.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Template Name: Impruw Home Page
+ */
+get_header();
+?>
+
+<div class="aj-imp-homepage">
+    
+    <div class="home-1">
+    	<h1>Grow Your Online Hotel Business<br> With <span>Impruw</span></h1>
+    	<a class="btn btn-hg big-sign"><span class="icon icon-pen"></span> Sign up for a free trial</a>
+    	<img src="<?php echo get_template_directory_uri(); ?>/images/HOME_banner.png" title="Impruw Themes" alt="Impruw Themes" class="img-responsive" />
+    </div>
+
+    <div class="home-2">
+    	<div class="row">
+    		<div class="col-sm-4">
+    			<img src="<?php echo get_template_directory_uri(); ?>/images/video.png" title="Impruw Themes" alt="Impruw Themes" class="img-responsive" />
+    		</div>
+    		<div class="col-sm-8">
+    			<h2>Our Cloud Software Provides</h2>
+    			<p>
+    				Businesses run succesfully on Impruw across the world. You can set up a website to showcase your hotel in less than an hour and give your local business the coverage it deserves!
+    			</p>
+    			<p>
+    				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Praesent id metus massa, ut blandit odio. Proin quis tortor orci. 
+    			</p>
+    		</div>
+    	</div>
+    	<div class="info-blocks clearfix">
+    		<div class="block">
+    			A Well Crafted Website that works across mobile, tablets and desktop.
+    		</div>
+    		<div class="block">
+    			Room Bookings and Availability Calendar.
+    		</div>
+    		<div class="block">
+    			Professional email service and other tools to manage your online presence.
+    		</div>
+    		<div class="block">
+    			Support for Multiple Languages
+    		</div>
+    		<div class="block">
+    			And truly Do It Yourself. Updating your website has never been this easy.
+    		</div>
+    	</div>
+    </div>
+
+    <div class="home-3">
+    	<h2>Do it yourself - Really!</h2>
+    	<p>Our simple drag and drop interface makes building beautiful free websites quick and simple.</p>
+    	<img src="<?php echo get_template_directory_uri(); ?>/images/HOME_gif.png" title="Impruw Site Builder" alt="Impruw Site Builder" class="img-responsive" />
+    </div>
+
+    <div class="home-4">
+    	<h2>Lorem Ipsum Dolor Sit Amet!</h2>
+    	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    	<div class="info-blocks clearfix">
+    		<div class="block row">
+    			<div class="col-sm-2 icon-holder">
+    				<span class="icon icon-magnifier"></span>
+    			</div>
+    			<div class="col-sm-10">
+    				<h4>Search Engine Friendly</h4>
+    				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+    			</div>
+    		</div>
+    		<div class="block row">
+    			<div class="col-sm-2 icon-holder">
+    				<span class="icon icon-tools"></span>
+    			</div>
+    			<div class="col-sm-10">
+    				<h4>Norway and London Based Support Team</h4>
+    				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+    			</div>
+    		</div>
+    		<div class="block row">
+    			<div class="col-sm-2 icon-holder">
+    				<span class="icon icon-wallet"></span>
+    			</div>
+    			<div class="col-sm-10">
+    				<h4>Friendly to your Pocket</h4>
+    				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+    			</div>
+    		</div>
+    		<div class="block row">
+    			<div class="col-sm-2 icon-holder">
+    				<span class="icon icon-box"></span>
+    			</div>
+    			<div class="col-sm-10">
+    				<h4>Works out of the box</h4>
+    				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+
+    <div class="home-5">
+    	<h2>Lorem Ipsum Dolor Sit Amet!</h2>
+    	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    	<img src="<?php echo get_template_directory_uri(); ?>/images/HOME_devices.png" title="Impruw Site Builder" alt="Impruw Site Builder" class="img-responsive" />
+    </div>
+
+    <div class="home-6">
+    	<div class="row">
+    		<div class="col-sm-6">
+    			<h4>Ready to get Started?</h4>
+    		</div>
+    		<div class="col-sm-6">
+    			<a class="btn btn-hg big-sign"><span class="icon icon-pen"></span> Sign up for a free trial</a>
+    		</div>
+    	</div>
+    </div>
+
+</div>
+
+<?php get_footer();
