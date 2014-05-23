fdfeebc78fc13491f6ac5e2a355d0cf54d5a2b6e
Jarryd <jarryd@ajency.in>
2014-05-23 15:31:39 +0530

    Contact Page Template and Styles

---
+++ wp-content/themes/impruwmain/page-contactpage.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template Name: Impruw Contact Page
+ */
+get_header();
+?>
+
+<div class="aj-imp-contactpage">
+    
+    <h1 class="main-title">You can find us at the following locations.</h1>
+
+    <div class="contact london">
+        <h2>London</h2>
+        <div class="c-address">
+            <b><?php echo __('Impruw Pvt. Ltd.', 'impruwmain'); ?></b><br>
+            <?php echo __('Leilighet 425', 'impruwmain'); ?><br>
+            <?php echo __('Trondheim', 'impruwmain'); ?><br>
+            <?php echo __('Norway', 'impruwmain'); ?><br>
+            <?php echo __('NO-7321', 'impruwmain'); ?><br>
+            Call Us: <a href="tel:+4712345678"><?php echo __('+47 - 12 34 56 78', 'impruwmain'); ?></a>
+        </div>
+        <div class="c-map">
+            <div class='embed-container'><iframe src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1784.5915336701821!2d10.400091!3d63.430284!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x466d319bd5506741%3A0xc19dc194656d7c78!2sVitensenteret+i+Trondheim!5e0!3m2!1sen!2s!4v1400835543532' width='800' height='600' frameborder='0' style='border:0'></iframe></div>
+        </div>
+    </div>
+
+    <div class="contact norway">
+        <h2>Norway</h2>
+        <div class="c-address">
+            <b><?php echo __('Impruw Pvt. Ltd.', 'impruwmain'); ?></b><br>
+            <?php echo __('Leilighet 425', 'impruwmain'); ?><br>
+            <?php echo __('Trondheim', 'impruwmain'); ?><br>
+            <?php echo __('Norway', 'impruwmain'); ?><br>
+            <?php echo __('NO-7321', 'impruwmain'); ?><br>
+            Call Us: <a href="tel:+4712345678"><?php echo __('+47 - 12 34 56 78', 'impruwmain'); ?></a>
+        </div>
+        <div class="c-map">
+            <div class='embed-container'><iframe src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1784.5915336701821!2d10.400091!3d63.430284!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x466d319bd5506741%3A0xc19dc194656d7c78!2sVitensenteret+i+Trondheim!5e0!3m2!1sen!2s!4v1400835543532' width='800' height='600' frameborder='0' style='border:0'></iframe></div>
+        </div>
+    </div>
+
+    <div class="clearfix"></div>
+
+</div>
+
+<?php get_footer();
