2d0d081373804afb6859468c6d78559f8049038c
Jarryd <jarryd@ajency.in>
2014-05-23 19:07:26 +0530

    FAQ Page Template and Styles

---
+++ wp-content/themes/impruwmain/page-faqpage.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Template Name: Impruw FAQ Page
+ */
+get_header();
+?>
+
+<div class="aj-imp-faqpage">
+    
+    <h1 class="main-title">Need some Help?</h1>
+
+    <p class="desc">
+        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt. 
+    </p>
+
+    <div class="search-support">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="form-group">
+                    <div class="input-group input-group-hg input-group-rounded">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="submit"><span class="fui-search"></span></button>
+                        </span>              
+                        <input type="text" id="search-query-2" placeholder="Search the FAQ's" class="form-control">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="faqs">
+        <div class="question">
+            What is Impruw?
+        </div>
+        <div class="answer">
+            Impruw is the best site builder in the world. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt. 
+        </div>
+
+        <div class="question">
+            What is Impruw?
+        </div>
+        <div class="answer">
+            Impruw is the best site builder in the world. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt. 
+        </div>
+
+        <h4 class="category-head">Another Topic of Discussion</h4>
+        <div class="question">
+            What is Impruw?
+        </div>
+        <div class="answer">
+            Impruw is the best site builder in the world. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt. 
+        </div>
+    </div>
+
+</div>
+
+<?php get_footer();
