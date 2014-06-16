97495b7f19fde67330ce22429d5bc0ef75a6a9c9
Jarryd <jarryd@ajency.in>
2014-06-16 18:26:50 +0530

    Added Default Language and Pick Language Markup and Styles

--- wp-content/themes/pink-theme/js/theme.js
+++ wp-content/themes/pink-theme/js/theme.js
@@ -4,13 +4,20 @@
 jQuery(window).resize(function() { 
 	var f = jQuery('.site-footer').height();
 	var h = jQuery('.site-style-container').height() - (f);
+	var lbh = jQuery('.long-bar > .column').css('min-height');
+
 	jQuery('.long-bar > .column').height(h);
 	jQuery('#jPanelMenu-menu').height(h);
+
+	if (h < 500) {
+		jQuery('.site-page').height(lbh);
+	}
 });
 
 jQuery(window).load(function() { 
 	var f = jQuery('.site-footer').height();
 	var h = jQuery('.site-style-container').height() - (f);
+	var lbh = jQuery('.long-bar > .column').height;
 	
 	setTimeout(function(){
 		jQuery(window).resize();
