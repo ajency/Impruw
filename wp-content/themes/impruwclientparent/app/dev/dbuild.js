2a371552ae149ace7aa56db50a594196d3fb0b31
Suraj Air <suraj@ajency.in>
2014-07-15 11:21:55 +0530

    ckeditor build fixed

--- wp-content/themes/impruwclientparent/app/dev/dbuild.js
+++ wp-content/themes/impruwclientparent/app/dev/dbuild.js
@@ -1,89 +1,89 @@
 ({
-  baseUrl: './js',
-  name: 'plugins/almond',
-  include : 'dashboard-main',
-  exclude: ['plugins/ckeditor'],
-  //optimize: 'none',
-  wrap: false,
-  out: '../production/dashboard-main.js',
-  paths: {
-    jquery: 'plugins/jquery',
-    jqueryui: 'plugins/jquery.ui',
-    underscore: 'plugins/underscore',
-    backbone: 'plugins/backbone',
-    marionette: 'plugins/backbone.marionette',
-    tpl: 'plugins/tpl',
-    text: 'plugins/text',
-    spin: 'plugins/spin',
-    jqueryspin: 'plugins/jquery.spin',
-    bootstrap: 'plugins/bootstrap',
-    moment: 'plugins/moment',
-    bootstrapselect: 'plugins/bootstrapselect',
-    underscorestring: 'plugins/underscore.string',
-    mustache: 'plugins/Mustache',
-    plupload: 'plugins/plupload.full',
-    d3: 'plugins/d3.v3',
-    nvd3: 'plugins/nv.d3',
-    chart : 'plugins/chart',
-    isotope: 'plugins/isotope',
-    radio: 'plugins/flatui-radio',
-    checkbox: 'plugins/flatui-checkbox',
-    backboneform: 'plugins/backbone.form',
-    backbonesyphon: 'plugins/backbone.syphon',
-    backboneassociations: 'plugins/backbone.associations',
-    jqueryvalidate: 'plugins/jquery.validate',
-    polyglot: 'plugins/polyglot',
-    app: 'dashboard-app',
-    jpanelmenu  : 'plugins/jquery.jpanelmenu.min',
-    bootstrapswitch: 'plugins/bootstrap-switch',
-    entitiesloader: 'entities/dashboard-entities-loader',
-    scrollsections: 'plugins/ajency.scrolldots',
-    minicolors: 'plugins/jquery.minicolors.min',
-    additionalmethod  : 'plugins/validate.additional.methods',
-    timepicker      : 'plugins/jquery.timepicker.min',
-    ckeditor: 'plugins/ckeditor'
-  },
-  shim: {
-    underscore: {
-      exports: '_'
+    baseUrl : './js',
+    name : 'plugins/almond',
+    include : 'dashboard-main',
+    exclude : ['ckeditor'],
+    //optimize: 'none',
+    wrap : false,
+    out : '../production/dashboard-main.js',
+    paths : {
+        jquery : 'plugins/jquery',
+        jqueryui : 'plugins/jquery.ui',
+        underscore : 'plugins/underscore',
+        backbone : 'plugins/backbone',
+        marionette : 'plugins/backbone.marionette',
+        tpl : 'plugins/tpl',
+        text : 'plugins/text',
+        spin : 'plugins/spin',
+        jqueryspin : 'plugins/jquery.spin',
+        bootstrap : 'plugins/bootstrap',
+        moment : 'plugins/moment',
+        bootstrapselect : 'plugins/bootstrapselect',
+        underscorestring : 'plugins/underscore.string',
+        mustache : 'plugins/Mustache',
+        plupload : 'plugins/plupload.full',
+        d3 : 'plugins/d3.v3',
+        nvd3 : 'plugins/nv.d3',
+        chart : 'plugins/chart',
+        isotope : 'plugins/isotope',
+        radio : 'plugins/flatui-radio',
+        checkbox : 'plugins/flatui-checkbox',
+        backboneform : 'plugins/backbone.form',
+        backbonesyphon : 'plugins/backbone.syphon',
+        backboneassociations : 'plugins/backbone.associations',
+        jqueryvalidate : 'plugins/jquery.validate',
+        polyglot : 'plugins/polyglot',
+        app : 'dashboard-app',
+        jpanelmenu : 'plugins/jquery.jpanelmenu.min',
+        bootstrapswitch : 'plugins/bootstrap-switch',
+        entitiesloader : 'entities/dashboard-entities-loader',
+        scrollsections : 'plugins/ajency.scrolldots',
+        minicolors : 'plugins/jquery.minicolors.min',
+        additionalmethod : 'plugins/validate.additional.methods',
+        timepicker : 'plugins/jquery.timepicker.min',
+        ckeditor : 'plugins/ckeditor'
     },
-    jquery: ['underscore'],
-    jqueryui: ['jquery'],
-    backbone: {
-      deps: ['jquery', 'underscore'],
-      exports : 'Backbone'
-    },
-    marionette: {
-      deps: ['backbone'],
-      exports : 'Marionette'
-    },
-    polyglot: {
-      exports: 'Polyglot'
-    },
-    plupload: {
-      deps: ['jquery'],
-      exports: 'plupload'
-    },
-    nvd3: {
-      deps: ['d3'],
-      exports: 'nv'
-    },
-    jqueryvalidate: ['jquery'],
-    jpanelmenu: ['jquery'],
-    minicolors      : ['jquery'],
-    additionalmethod  : ['jquery','jqueryvalidate'],
-    timepicker      : ['jquery'],
-    underscorestring: ['underscore'],
-    backboneform: ['backbone'],
-    backbonesyphon: ['backbone'],
-    backboneassociations: ['backbone'],
-    jqueryspin: ['spin'],
-    bootstrap: ['jquery'],
-    isotope: ['jquery'],
-    radio: ['bootstrap'],
-    checkbox: ['bootstrap'],
-    bootstrapselect: ['bootstrap'],
-    bootstrapswitch: ['bootstrap'],
-    app: ['plugins/dashboard-plugin-loader', 'configs/dashboard-config-loader']
-  },
+    shim : {
+        underscore : {
+            exports : '_'
+        },
+        jquery : ['underscore'],
+        jqueryui : ['jquery'],
+        backbone : {
+            deps : ['jquery', 'underscore'],
+            exports : 'Backbone'
+        },
+        marionette : {
+            deps : ['backbone'],
+            exports : 'Marionette'
+        },
+        polyglot : {
+            exports : 'Polyglot'
+        },
+        plupload : {
+            deps : ['jquery'],
+            exports : 'plupload'
+        },
+        nvd3 : {
+            deps : ['d3'],
+            exports : 'nv'
+        },
+        jqueryvalidate : ['jquery'],
+        jpanelmenu : ['jquery'],
+        minicolors : ['jquery'],
+        additionalmethod : ['jquery', 'jqueryvalidate'],
+        timepicker : ['jquery'],
+        underscorestring : ['underscore'],
+        backboneform : ['backbone'],
+        backbonesyphon : ['backbone'],
+        backboneassociations : ['backbone'],
+        jqueryspin : ['spin'],
+        bootstrap : ['jquery'],
+        isotope : ['jquery'],
+        radio : ['bootstrap'],
+        checkbox : ['bootstrap'],
+        bootstrapselect : ['bootstrap'],
+        bootstrapswitch : ['bootstrap'],
+        app : ['plugins/dashboard-plugin-loader', 'configs/dashboard-config-loader']
+    }
 })
--- wp-content/themes/impruwclientparent/new-dashboard.php
+++ wp-content/themes/impruwclientparent/new-dashboard.php
@@ -79,6 +79,7 @@ echo json_encode($impruwUserModel->get_user_basic_info());
     JSVERSION ?>"></script>
 <?php else: ?>
     <!--<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script> -->
+    <script src="<?php echo get_parent_template_directory_uri(); ?>/app/dev/js/plugins/ckeditor.js"></script>
     <script data-main="<?php echo get_parent_template_directory_uri(); ?>/app/dev/js/dashboard-main"
             src="<?php echo get_parent_template_directory_uri(); ?>/dashboard/require.js"></script>
 <?php endif; ?>
