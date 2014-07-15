e14ff2631d0e16c150220dd6b23c8f3981c8a6bb
Jarryd <jarryd@ajency.in>
2014-07-15 18:03:59 +0530

    Recompiled JS

--- wp-content/themes/impruwclientparent/app/dev/js/apps/rooms/edit/views.js
+++ wp-content/themes/impruwclientparent/app/dev/js/apps/rooms/edit/views.js
@@ -1,7 +1,7 @@
 var __hasProp = {}.hasOwnProperty,
   __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-define(['app', 'text!apps/rooms/edit/templates/edit-room.html'], function(App, addRoomTpl) {
+define(['app', 'text!apps/rooms/edit/templates/edit-room.html', 'jqueryvalidate'], function(App, addRoomTpl, jqueryvalidate) {
   return App.module('RoomsApp.Edit.View', function(View, App, Backbone, Marionette, $, _) {
     return View.EditRoomLayout = (function(_super) {
       __extends(EditRoomLayout, _super);
@@ -34,12 +34,26 @@ define(['app', 'text!apps/rooms/edit/templates/edit-room.html'], function(App, a
 
       EditRoomLayout.prototype.onShow = function() {
         var m, w;
+        this.$el.find('form').validationOptions();
         this.$el.find('*[data-spy="affix"]').affix();
         w = $('.aj-imp-right').width();
         this.$el.find('*[data-spy="affix"]').width(w);
         m = $('.aj-imp-left').width();
         this.$el.find('*[data-spy="affix"]').css('margin-left', m);
         return this.$el.find('.currency').text(Marionette.getOption(this, 'currency'));
+      };
+
+      EditRoomLayout.prototype.validationOptions = function() {
+        return {
+          rules: {
+            no_of_rooms: {
+              number: true
+            }
+          },
+          messages: {
+            no_of_rooms: 'Enter valid url'
+          }
+        };
       };
 
       EditRoomLayout.prototype.onShowSuccessMessage = function() {
