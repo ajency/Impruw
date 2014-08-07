04236fffb3b4ac1c56987bfaa661e0cf067ce09a
Jarryd <jarryd@ajency.in>
2014-08-07 16:29:52 +0530

    Changed Room Description Default Builder Text and Element Name #229

--- wp-content/themes/impruwclientparent/app/dev/js/apps/builder/site-builder/elements/roomdescription/views.js
+++ wp-content/themes/impruwclientparent/app/dev/js/apps/builder/site-builder/elements/roomdescription/views.js
@@ -12,7 +12,7 @@ define(['app'], function(App) {
 
       RoomDescriptionView.prototype.className = 'roomdescription';
 
-      RoomDescriptionView.prototype.template = '<div class="room-description-container clearfix"> <div class="room-description"> <h1>Room Description</h1> <div class="room-description-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div> </div> </div>';
+      RoomDescriptionView.prototype.template = '<div class="room-description-container clearfix"> <div class="room-description"> <h1>Room Description</h1> <div class="room-description-desc">{{#polyglot}}This will display the room description you added to the room. You cannot edit your room description here. To edit the room description, go to Rooms from Dashboard and edit your room description there. The text you add there will display on the live site. You cannot add links and formatting to your room description. The format in which the text is displayed will be defined by the theme you are using. To display your check in / check out time and additional policies, drag and drop the Room Summary element to your single room page.{{/polyglot}}</div> </div> </div>';
 
       RoomDescriptionView.prototype.onShow = function() {
         this.$el.attr("data-content", "Update room information <a href='" + SITEURL + "/dashboard/#/rooms'>here</a> ");
