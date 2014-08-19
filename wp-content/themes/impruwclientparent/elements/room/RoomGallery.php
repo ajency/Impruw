2f2e72a515f6b55287d8c99c312522e2aedd4ab3
Jarryd <jarryd@ajency.in>
2014-08-19 14:05:21 +0530

    Fixed Single Room Gallery Display #274

--- wp-content/themes/impruwclientparent/elements/room/RoomGallery.php
+++ wp-content/themes/impruwclientparent/elements/room/RoomGallery.php
@@ -45,8 +45,13 @@ class RoomGallery extends SliderElement {
     function __construct($element, $post_id = 0) {
        
         //parent::__construct($element );
+        var_dump($this->get_slider_id());
             
-        $this->slider_id = isset($element['slider_id']) ? $element['slider_id'] : $this->get_slider_id();
+        if(!is_singular('impruw_room')) {
+          $this->slider_id = $element['slider_id'];
+        } else {
+          $this->slider_id = $this->get_slider_id();
+        } 
         
         if((int)$this->slider_id !== 0){
           $this->markup    =    $this->generate_markup();
