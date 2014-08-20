0a6a5d447f1e55c983fca12083efaf15d5eea594
Jarryd <jarryd@ajency.in>
2014-08-20 14:46:26 +0530

    Added Theme Preview Color Picker

--- wp-content/themes/impruwclientparent/footer.php
+++ wp-content/themes/impruwclientparent/footer.php
@@ -10,7 +10,6 @@
  */
 ?>
 
-
 <footer class="site-footer">
     <?php echo generate_markup( 'footer' ); ?>
 </footer><!-- .site-footer -->
@@ -65,6 +64,111 @@
     }
     google.maps.event.addDomListener(window, 'load', initialize);
 </script>
+
+<!-- Site Preview Options -->
+<?php if(isset($_GET['preview'])):?>
+
+    <script src="<?php echo get_parent_template_directory_uri(); ?>/app/dev/js/plugins/jquery.tabSlideOut.v1.3.js"></script>
+    <script type="text/javascript">
+        jQuery(function(){
+            jQuery('.options-div').tabSlideOut({
+                tabHandle: '.handle',                     //class of the element that will become your tab
+                tabLocation: 'left',                      //side of screen where tab lives, top, right, bottom, or left
+                speed: 300,                               //speed of animation
+                action: 'click',                          //options: 'click' or 'hover', action to trigger animation
+                topPos: '150px',                          //position from the top/ use if tabLocation is left or right
+                fixedPosition: true                       //options: true makes it stick(fixed position) on scroll
+            });
+        });
+    </script>
+    <style type="text/css">
+        .options-div {
+            background: #333;
+            width: 200px;
+            padding: 0.5em;
+            color: #fff;
+            z-index: 9999;
+            border-radius: 0 0 2px 0;
+        }
+        .options-div .handle {
+            background: #333;
+            padding: 0.4em 0.5em 0.5em;
+            color: #fff;
+            font-size: 1.5em;
+            border-radius: 0 2px 2px 0;
+        }
+        .options-div .handle:hover {
+            color: #FF7E00
+        }
+        .options-div h5 {
+            font-size: 1.2em;
+            text-transform: uppercase;
+        }
+        .options-div .option-colors {
+            list-style: none;
+            padding: 0;
+        }
+        .options-div .option-colors li a {
+            display: block;
+            margin-bottom: 0.5em;
+            color: #ccc;
+            padding: 0.5em;
+            border-radius: 2px;
+        }
+        .options-div .option-colors li a:hover, .options-div .option-colors li a.active {
+            background: #222;
+            color: #fff;
+        }
+        .options-div .option-colors li a h6 {
+            font-weight: 300;
+            font-size: 0.8em;
+        }
+        .options-div .option-colors li a .color {
+            display: inline-block;
+            width: 15px;
+            height: 15px;
+            border: 1px solid #fff;
+            margin: 0 0.2em 0.3em 0;
+        }
+    </style>
+
+    <div class="options-div">
+        <a class="handle" href="#"><span class="glyphicon glyphicon-cog"></span></a>
+        <h5>Color Options</h5>
+        <ul class="option-colors">
+            <li>
+                <a href="#" class="active">
+                    <h6>Default</h6>
+                    <span class="color" style="background: #24D07C;"></span>
+                    <span class="color" style="background: #E223A1;"></span>
+                    <span class="color" style="background: #39435C;"></span>
+                    <span class="color" style="background: #545D73;"></span>
+                    <span class="color" style="background: #E1E1E1;"></span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <h6>Lemon Dash</h6>
+                    <span class="color" style="background: #ECFF33;"></span>
+                    <span class="color" style="background: #212121;"></span>
+                    <span class="color" style="background: #2F8F74;"></span>
+                    <span class="color" style="background: #A0FB70;"></span>
+                    <span class="color" style="background: #E1E1E1;"></span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <h6>Desert Song</h6>
+                    <span class="color" style="background: #72451E;"></span>
+                    <span class="color" style="background: #E7512E;"></span>
+                    <span class="color" style="background: #B6AEA1;"></span>
+                    <span class="color" style="background: #CBCBC1;"></span>
+                    <span class="color" style="background: #E1E1E1;"></span>
+                </a>
+            </li>
+        </ul>
+    </div>
+<?php endif; ?>
 
 <?php wp_footer(); ?>
 </body>
