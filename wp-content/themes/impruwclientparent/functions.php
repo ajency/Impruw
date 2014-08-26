3ecbde36bb6aa256b8877e8f5bb0d8b86e9e606e
Suraj Air <suraj@ajency.in>
2014-08-26 18:00:56 +0530

    extra lightbox plugin removed

--- wp-content/themes/impruwclientparent/functions.php
+++ wp-content/themes/impruwclientparent/functions.php
@@ -926,7 +926,6 @@ function get_theme_JS() {
     <script src="<?php echo get_parent_template_directory_uri(); ?>/js/user_management.js"></script>
     <!-- Isotope -->
     <script src="<?php echo get_parent_template_directory_uri(); ?>/app/dev/js/plugins/isotope.js"></script>
-    <script src="<?php echo get_parent_template_directory_uri(); ?>/js/lightbox.js"></script>
     <script>
         jQuery(document).ready(function () {
             if (jQuery('ul.gallery li').length === 0)
@@ -992,9 +991,6 @@ function get_theme_CSS() {
         type="text/css" rel="stylesheet"/>
     <link
         href="<?php echo get_parent_template_directory_uri(); ?>/css/flat-ui.css"
-        type="text/css" rel="stylesheet"/>
-    <link
-        href="<?php echo get_parent_template_directory_uri(); ?>/css/lightbox.css"
         type="text/css" rel="stylesheet"/>
     <link
         href="<?php echo get_parent_template_directory_uri(); ?>/css/style.css"
