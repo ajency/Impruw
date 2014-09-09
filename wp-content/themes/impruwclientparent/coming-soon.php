1461c39f8ab29f34f08e42ec1f0e7a06bb4fb75c
Nutan Kamat <nutan@ajency.in>
2014-09-09 13:11:49 +0530

    coming soon page translation quick fix

--- wp-content/themes/impruwclientparent/coming-soon.php
+++ wp-content/themes/impruwclientparent/coming-soon.php
@@ -5,7 +5,17 @@
 ?>
 <html>
 <head>
-    <title><?php echo get_bloginfo( 'name' ); ?> is Coming Soon!</title>
+    <?php 
+        $coming_soon_text = 'is Coming Soon!';
+        $default_language = wpml_get_default_language();
+
+        if($default_language==='nb')
+            $coming_soon_text = 'kommer snart !';
+        else
+            $coming_soon_text = 'is Coming Soon!';
+
+    ?>
+    <title><?php echo get_bloginfo( 'name' ); ?> <?php echo $coming_soon_text; ?></title>
     <style type="text/css">
         html, body {
             margin: 0;
@@ -48,16 +58,6 @@
 </head>
 <body>
 <div class="coming-soon">
-    <?php 
-        $coming_soon_text = 'is Coming Soon!';
-        $default_language = wpml_get_default_language();
-
-        if($default_language==='nb')
-            $coming_soon_text = 'kommer snart !';
-        else
-            $coming_soon_text = 'is Coming Soon!';
-
-    ?>
     <h1><span><?php echo get_bloginfo( 'name' ); ?></span> <?php echo $coming_soon_text; ?></h1>
     <footer class="fixed">
         &copy;Impruw 2014 | All Rights Reserved.
