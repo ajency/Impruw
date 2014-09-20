0e8066697072246c4c9cae7493165b140d0bf0d5
Jarryd <jarryd@ajency.in>
2014-09-20 14:47:15 +0530

    Markup and Styling for 404 Page #416

--- wp-content/themes/impruwclientparent/404.php
+++ wp-content/themes/impruwclientparent/404.php
@@ -1,8 +1,37 @@
-<html>
-<head>
-
-</head>
-<body>
-<h2>404 found</h2>
-</body>
-</html
+<?php
+/**
+ * Template Name: 404
+ */
+get_header();
+?>
+    <div class="aj-imp-login-form">
+        <div class="row">
+            <div class="col-sm-12 aj-imp-login-header">
+                <h1><?php echo __( 'Uh Oh!', 'impruwmain' ); ?>
+                    <span><?php echo __( 'Not Found.', 'impruwmain' ); ?></span>
+                </h1>
+                <p class="desc">
+                    <?php echo __( 'We could not find what you were looking for.', 'impruwmain' ); ?>
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 aj-imp-login btn-home">
+            	<span class="link button action-button">
+	            	<a href="<?php echo site_url(); ?>"><?php echo __( 'Go Home', 'impruwmain' ); ?></a>
+	            </span>
+            </div>
+        </div>
+    </div>
+    <style type="text/css">
+    	.btn-home {
+    		text-align: center;
+    	}
+    </style>
+    <script type="text/javascript">
+        jQuery(document).ready(function () {
+            jQuery('.error404.logged-in').css({'visibility': 'visible'})
+        });
+    </script>
+<?php
+get_footer();
