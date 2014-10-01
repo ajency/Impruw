f0703c6c924aeabf5a497766e8c932a4354436e1
Elroy Antao <elroy@ajency.in>
2014-10-01 16:32:13 +0530

    mobile preview 5 sizes added

--- wp-content/themes/impruwclientparent/index.php
+++ wp-content/themes/impruwclientparent/index.php
@@ -23,10 +23,14 @@ if ( isset( $_GET[ 'sim' ] ) ) {
 	// $url =  substr($actual_link, 0,-9);
 	$url = str_replace("&sim=full", "", $actual_link);
 	$url = str_replace("?sim=full", "", $url);
-	$url = str_replace("&sim=tab", "", $url);
-	$url = str_replace("?sim=tab", "", $url);
-	$url = str_replace("&sim=mob", "", $url);
-	$url = str_replace("?sim=mob", "", $url);
+	$url = str_replace("&sim=tabl", "", $url);
+	$url = str_replace("?sim=tabl", "", $url);
+	$url = str_replace("&sim=tabp", "", $url);
+	$url = str_replace("?sim=tabp", "", $url);
+	$url = str_replace("&sim=mobl", "", $url);
+	$url = str_replace("?sim=mobl", "", $url);
+	$url = str_replace("&sim=mobp", "", $url);
+	$url = str_replace("?sim=mobp", "", $url);
 
 	// print_r($url);
 	?>
@@ -38,52 +42,56 @@ if ( isset( $_GET[ 'sim' ] ) ) {
 	<html class="ie ie8" <?php language_attributes(); ?>>
 	<![endif]-->
 	<!--[if !(IE 7) | !(IE 8)  ]><!-->
-	<html <?php language_attributes(); ?> style="height:100%">
+	<html <?php language_attributes(); ?> >
 	<!--<![endif]-->
 	<head>
 	    <meta charset="<?php bloginfo( 'charset' ); ?>">
 	    <meta name="viewport" content="width=device-width">
-	    <script type="text/javascript" src="<?php echo site_url(); ?>/wp-includes/js/jquery/jquery.js?ver=1.11.0"></script>
-	    <script src="<?php echo get_parent_template_directory_uri(); ?>/app/dev/js/plugins/jquery.tabSlideOut.v1.3.js"></script>
-	   
+	    <script type="text/javascript" src="<?php echo site_url(); ?>/wp-includes/js/jquery/jquery.js?ver=1.11.0"></script>	   
+	  <!-- 
 	    <script type="text/javascript" src="<?php echo site_url(); ?>/wp-content/themes/impruwclientparent/js/preview-init.js"></script>
-	   
+	   --> 
 	    <style type="text/css">
 		    li{
 		    	display: inline;
 		    }
-		    .options-div {
-
-	            background: #333;
-	            width: 200px;
-	            padding: 0.5em;
-	            color: #fff;
-	            z-index: 9999;
-	            border-radius: 0 0 2px 0;
-	            height: auto !important;
-	        }
-	        .options-div .handle {
-	            background: #333;
-	            padding: 0.4em 0.5em 0.5em;
-	            color: #fff;
-	            font-size: 1.5em;
-	            border-radius: 0 2px 2px 0;
-	            /*right: 95px*/
-	        }
-	        .options-div.open .handle {
-	            right: -42px !important;
-	        }
-	        .options-div .handle:hover {
-	            color: #FF7E00
-	        }
-	        .options-div h5 {
-	            font-size: 1.2em;
-	            text-transform: uppercase;
-	        }
+		    body{
+		    	overflow: hidden;
+		    	margin: 0px;
+		    }
+		    #viewport{
+		    	margin-left:auto;
+		    	margin-right:auto;
+		    }
+		    ul{
+		    	margin-top: 0px;
+		    	margin-bottom: 0px
+		    }
+		    .options-div{
+		    	margin-top: 10px
+		    }
+		    iframe{
+		    	border: 0px;
+		    }
+		    
 	    </style>
+	    <script type="text/javascript">
+	    	jQuery(document).ready(function($) {
+	    		function setHeight(){
+	    			jQuery('#viewport').height( jQuery(window).height() - jQuery('.options-div').height() - 14);
+	    		}
+
+	    		jQuery(window).resize(function(){
+	    			setHeight();
+	    		});
+
+	    		setHeight();
+
+	    	});
+	    </script>
 
 	</head>
-	<body style="height:97%; margin:0px">
+	<body >
 
 		<div class="options-div">
         	<a class="handle" href="#"><span class="glyphicon glyphicon-cog"></span></a>
@@ -92,21 +100,31 @@ if ( isset( $_GET[ 'sim' ] ) ) {
 		        <li <?php if($sim == 'full') echo "class='active'"; ?> >
 		        	<a href="<?php echo $url ?>&sim=full" id="size-full" >Full</a>
 		        </li>
-		        <li <?php if($sim == 'tab') echo "class='active'"; ?> >
-		        	<a href="<?php echo $url ?>&sim=tab" id="size-tab">Tablet</a>
+		        <li <?php if($sim == 'tabl') echo "class='active'"; ?> >
+		        	<a href="<?php echo $url ?>&sim=tabl" id="size-tabl">Tablet-l</a>
 		        </li>
-		        <li <?php if($sim == 'mob') echo "class='active'"; ?> >
-		        	<a href="<?php echo $url ?>&sim=mob" id="size-mobile">Mobile</a>
+		        <li <?php if($sim == 'tabp') echo "class='active'"; ?> >
+		        	<a href="<?php echo $url ?>&sim=tabp" id="size-tabp">Tablet-p</a>
+		        </li>
+		        <li <?php if($sim == 'mobl') echo "class='active'"; ?> >
+		        	<a href="<?php echo $url ?>&sim=mobl" id="size-mobilel">Mobile-l</a>
+		        </li>
+		        <li <?php if($sim == 'mobp') echo "class='active'"; ?> >
+		        	<a href="<?php echo $url ?>&sim=mobp" id="size-mobilep">Mobile-p</a>
 		        </li>
 		    </ul>
 		</div>
 		<?php
 		if($sim == 'full')
-			echo '<div id="viewport" style="margin-left:auto; margin-right:auto; height:100%; width:100%; "> ';
-		elseif ($sim == 'tab')
-			echo '<div id="viewport" style="margin-left:auto; margin-right:auto; height:1024px; width:768px;">';
-		elseif ($sim == 'mob')
-			echo '<div id="viewport" style="margin-left:auto; margin-right:auto; height:480px; width:320px;">';		
+			echo '<div id="viewport" style="  width:100%; "> ';
+		elseif ($sim == 'tabl')
+			echo '<div id="viewport" style=" width:1024px;">';
+		elseif ($sim == 'tabp')
+			echo '<div id="viewport" style=" width:768px;">';
+		elseif ($sim == 'mobl')
+			echo '<div id="viewport" style=" width:480px;">';
+		elseif ($sim == 'mobp')
+			echo '<div id="viewport" style=" width:320px;">';		
 		?>
 			<iframe  src="<?php echo $url; ?>" style="height:100%; width:100%"></iframe>
 		</div>
