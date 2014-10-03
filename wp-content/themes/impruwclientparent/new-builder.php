fa8b69004aa8e58e2a541894770ad5c672c594b9
Elroy Antao <elroy@ajency.in>
2014-10-03 18:35:33 +0530

    map-fix

--- wp-content/themes/impruwclientparent/new-builder.php
+++ wp-content/themes/impruwclientparent/new-builder.php
@@ -125,7 +125,7 @@
     var ISDEMOTHEME = '<?php echo in_array(get_current_blog_id(), explode(',', THEME_ID)) ?>';
     var heartbeatSettings = <?php echo json_encode(wp_heartbeat_settings(array())); ?>;
 </script>
-<!-- <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script> -->
+<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
 <script src="<?php echo get_parent_template_directory_uri() ?>/app/dev/js/plugins/pace.js"></script>
 <!-- Unused Elements Box -->
 <div id="fl_menu" class="aj-imp-trash-elements"></div>
@@ -140,10 +140,11 @@
     <script
         src="<?php echo get_parent_template_directory_uri(); ?>/app/production/builder-main.js?ver=<?php echo JSVERSION ?>"></script>
 <?php else: ?>
+   <!-- 
       <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script> 
 
     <script src="<?php  get_parent_template_directory_uri(); ?>/app/dev/js/plugins/ckeditor.js"></script>
-
+ -->
     <script data-main="http://localhost/impruw/wp-content/themes/impruwclientparent/app/dev/js/builder-main"
             src="<?php echo get_parent_template_directory_uri(); ?>/js/require.js"></script>
 <?php endif; ?>
