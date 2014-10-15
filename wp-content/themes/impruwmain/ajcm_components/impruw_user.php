0b2da21cb7783d42c77334311c99b4c811c2acd6
Godfrey <godfrey@ajency.in>
2014-10-15 13:03:21 +0530

    fetching user_id from reciepients array & blog_id from comm_data

--- wp-content/themes/impruwmain/ajcm_components/impruw_user.php
+++ wp-content/themes/impruwmain/ajcm_components/impruw_user.php
@@ -62,16 +62,47 @@ function getvars_registration_email_user($recipients_email,$comm_data){
     $template_data['from_email'] = 'no-reply@impruw.in';
     $template_data['from_name'] = 'Impruw';
 
-    $username       = $aj_comm->get_communication_meta($comm_data['id'],'username');
-    $site_name      = $aj_comm->get_communication_meta($comm_data['id'],'site_name');
-    $reg_date       = $aj_comm->get_communication_meta($comm_data['id'],'date');
+    //$username       = $aj_comm->get_communication_meta($comm_data['id'],'username');
+   // $site_name      = $aj_comm->get_communication_meta($comm_data['id'],'site_name');
+    //$reg_date       = $aj_comm->get_communication_meta($comm_data['id'],'date');
     //$reg_time       = $aj_comm->get_communication_meta($comm_data['id'],'time');
 
+    $blog_details = get_blog_details($comm_data->blog_id);
+    $site_name = $blog_details->blogname;
+
     $template_data['global_merge_vars'] = array();
-    $template_data['global_merge_vars'][] = array('name' => 'USERNAME','content' => $username);
     $template_data['global_merge_vars'][] = array('name' => 'SITENAME','content' => $site_name);
-    $template_data['global_merge_vars'][] = array('name' => 'DATE','content' => $reg_date);
+
     // $template_data['global_merge_vars'][] = array('name' => 'TIME','content' => $reg_time);
+
+    foreach($recipients_email as $user_value){
+
+        $user = get_userdata($user_value->user_id );
+        $username = $user->user_nicename;
+        $reg_date = $user->user_registered;
+
+        $template_data['global_merge_vars'][] = array('name' => 'USERNAME','content' => $username);
+        $template_data['global_merge_vars'][] = array('name' => 'DATE','content' => $reg_date);
+
+
+        /*$overwrite_vars = array();
+
+        $user = get_userdata($user_value->user_id );
+        $admin_name = $user->display_name;
+
+        $overwrite_vars[] = array(
+            'name' => 'ADMINNAME',
+            'content' => $admin_name
+        );
+
+        $template_data['merge_vars'][] = array(
+
+            'rcpt'=>$user_value->value,
+            'vars' => $overwrite_vars
+        ) ;*/
+    }
+
+    update_option('Godfrey_debug',$comm_data);
 
     return $template_data;
 }
