9fc050cb23d8b4fb3ce580bd92277f0d17fcf39c
Nutan Kamat <nutan@ajency.in>
2014-10-17 10:50:39 +0530

    email view modification when email feature disabled

--- wp-content/themes/impruwclientparent/app/dev/js/apps/emails/email-nav/view.js
+++ wp-content/themes/impruwclientparent/app/dev/js/apps/emails/email-nav/view.js
@@ -36,6 +36,10 @@ define(['app'], function(App) {
 
       EmailDisabledView.prototype.template = '<div class="empty-info">You cannot use the emails feature since your domain name is not updated. Update your domain name by going to Site Profile on the Dashboard. Once you have changed your domain name, you can come back here to add email accounts for that domain.</div>';
 
+      EmailDisabledView.prototype.onShow = function() {
+        return $('.aj-imp-widget-container').hide();
+      };
+
       return EmailDisabledView;
 
     })(Marionette.ItemView);
--- wp-content/themes/impruwclientparent/app/dev/source/apps/emails/email-nav/view.coffee
+++ wp-content/themes/impruwclientparent/app/dev/source/apps/emails/email-nav/view.coffee
@@ -23,5 +23,8 @@ define ['app'], (App)->
 
                     template : '<div class="empty-info">You cannot use the emails feature since your domain name is not updated. Update your domain name by going to Site Profile on the Dashboard. Once you have changed your domain name, you can come back here to add email accounts for that domain.</div>'
 
+                    onShow:->
+                        $('.aj-imp-widget-container').hide()
+
 
 
