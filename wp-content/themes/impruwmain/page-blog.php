1875797f3cd8d14a74acda8bbcfaca6a6266042b
Jarryd <jarryd@ajency.in>
2014-11-17 19:03:22 +0530

    Blog Styling and Sidebar Markup #762

--- wp-content/themes/impruwmain/page-blog.php
+++ wp-content/themes/impruwmain/page-blog.php
@@ -7,6 +7,13 @@ get_header();
 
 	<div id="content" class="impruw-blog aj-imp-register-form">
 
+		<h1 class="main-title">
+	    	<?php the_title(); ?>
+	    </h1>
+
+	    <div class="row">
+	    	<div class="col-sm-9">
+
         <?php query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
 
 	<?php if( have_posts() ): ?>
@@ -16,14 +23,16 @@ get_header();
 	    <div id="post-<?php echo get_the_ID(); ?>" <?php post_class(); ?>>
 	    	<?php if ( has_post_thumbnail() ) { ?>
         		<a href="<?php the_permalink(); ?>" class="thumb">
-        			<?php the_post_thumbnail( 'large' ); ?>
+        			<?php the_post_thumbnail( 'large', array('class' => 'img-responsive') ); ?>
         		</a>
         	<?php } ?>
             <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
             <span class="meta"><strong><?php the_time('F jS, Y'); ?></strong> / <strong><?php the_author_link(); ?></strong> / <span class="comments"><?php comments_popup_link(__('0 comments','impruwmain'),__('1 comment','impruwmain'),__('% comments','impruwmain')); ?></span></span>
 
-		<?php the_excerpt(__('Continue reading »','impruwmain')); ?>
+			<?php the_excerpt(__('Read More','impruwmain')); ?>
+
+			<a href="<?php the_permalink(); ?>"><?php _e('Read More','impruwmain'); ?></a>
 
             </div><!-- /#post-<?php get_the_ID(); ?> -->
 
@@ -42,6 +51,15 @@ get_header();
 	    </div><!-- /#post-404 -->
 
 	<?php endif; wp_reset_query(); ?>
+			</div>
+
+			<div class="col-sm-3">
+				<div class="blog-sidebar">
+					Sidebar!
+				</div>
+			</div>
+
+		</div>
 
 	</div><!-- /#content -->
 
