fbb19ab91b1cd378150a1b2067107ff6f7bfe87d
Jarryd <jarryd@ajency.in>
2014-11-18 12:52:18 +0530

    Blog Sidebar Categories Added #762

--- wp-content/themes/impruwmain/page-blog.php
+++ wp-content/themes/impruwmain/page-blog.php
@@ -21,18 +21,22 @@ get_header();
         <?php while( have_posts() ): the_post(); ?>
 
 	    <div id="post-<?php echo get_the_ID(); ?>" <?php post_class(); ?>>
-	    	<?php if ( has_post_thumbnail() ) { ?>
+	    	
+            <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+            <span class="meta">
+            	Posted by <strong><?php the_author_link(); ?></strong> | on <strong><?php the_time('F jS, Y'); ?></strong> | <span class="comments"><?php comments_popup_link(__('No comments yet','impruwmain'),__('1 comment','impruwmain'),__('% comments','impruwmain')); ?></span>
+            </span>
+            <?php if ( has_post_thumbnail() ) { ?>
         		<a href="<?php the_permalink(); ?>" class="thumb">
         			<?php the_post_thumbnail( 'large', array('class' => 'img-responsive') ); ?>
         		</a>
         	<?php } ?>
-            <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <div class="excerpt">
+				<?php the_excerpt(__('Read More','impruwmain')); ?>
+			</div>
 
-            <span class="meta"><strong><?php the_time('F jS, Y'); ?></strong> / <strong><?php the_author_link(); ?></strong> / <span class="comments"><?php comments_popup_link(__('0 comments','impruwmain'),__('1 comment','impruwmain'),__('% comments','impruwmain')); ?></span></span>
-
-			<?php the_excerpt(__('Read More','impruwmain')); ?>
-
-			<a href="<?php the_permalink(); ?>"><?php _e('Read More','impruwmain'); ?></a>
+			<a href="<?php the_permalink(); ?>" class="more-link"><?php _e('Read More','impruwmain'); ?></a>
 
             </div><!-- /#post-<?php get_the_ID(); ?> -->
 
@@ -55,7 +59,37 @@ get_header();
 
 			<div class="col-sm-3">
 				<div class="blog-sidebar">
-					Sidebar!
+					<ul class="cat-list">
+						<?php 
+						    $args = array(
+							'show_option_all'    => '',
+							'orderby'            => 'name',
+							'order'              => 'ASC',
+							'style'              => 'list',
+							'show_count'         => 0,
+							'hide_empty'         => 1,
+							'use_desc_for_title' => 1,
+							'child_of'           => 0,
+							'feed'               => '',
+							'feed_type'          => '',
+							'feed_image'         => '',
+							'exclude'            => '',
+							'exclude_tree'       => '',
+							'include'            => '',
+							'hierarchical'       => 1,
+							'title_li'           => __( 'Categories' ),
+							'show_option_none'   => __( 'No categories' ),
+							'number'             => null,
+							'echo'               => 1,
+							'depth'              => 0,
+							'current_category'   => 0,
+							'pad_counts'         => 0,
+							'taxonomy'           => 'category',
+							'walker'             => null
+						    );
+						    wp_list_categories( $args ); 
+						?>
+					</ul>
 				</div>
 			</div>
 
