8af53c8b50aeabccd5ed72e2cb9edc13fa8f7a49
Nutan Kamat <nutan@ajency.in>
2014-12-13 13:08:23 +0530

    move payment specific code in payment-plans module

--- wp-content/themes/impruwclientparent/functions.php
+++ wp-content/themes/impruwclientparent/functions.php
@@ -43,6 +43,7 @@ include_once( dirname( __FILE__ ) . '/modules/elements/ajax.php' );
 include_once( dirname( __FILE__ ) . '/modules/media/ajax.php' );
 include_once( dirname( __FILE__ ) . '/modules/language/ajax.php' );
 include_once( dirname( __FILE__ ) . '/modules/language/languagefunctions.php' );
+include_once( dirname( __FILE__ ) . '/modules/payment-plans/functions.php' );
 include_once( dirname( __FILE__ ) . '/modules/billing/ajax.php' );
 include_once( dirname( __FILE__ ) . '/modules/seo/ajax.php' );
 include_once( dirname( __FILE__ ) . '/modules/emails/ajax.php' );
@@ -4014,26 +4015,3 @@ function show_revision_footer_placeholder(){
 
 }
 
-/**
- * Function to add site features that will be used by the aj_billing plugin to create plans for the site
- * Features can be of 2 types currently : 
- *  1. yes_no_type - those that can only be enabled or disabled - eg. Domain mapping
- *  2. count_type - those that can be enabled/disabled as well as have a count associated to them - eg. Email accounts
- * Each feature has a name and a key. the key is a unique single word and will be used to refer the feature
- */
-function impruw_add_feature_components($defined_feature_components){
-
-    $yes_no_features = array(
-        array('key' =>'domain_mapping' , 'name' => 'Domain Mapping' )
-    );
-
-    $count_features =  array(array('key' =>'email_account' , 'name' => 'Email Account' ),
-    array('key'=> 'site_add_ons', 'name' => 'Site Add Ons' ));
-
-    $feature_components['yes_no_type'] = $yes_no_features;
-    $feature_components['count_type'] = $count_features;
-
-    return $feature_components;
-
-}
-add_filter('add_feature_components_filter','impruw_add_feature_components',10,1);
---
+++ wp-content/themes/impruwclientparent/modules/payment-plans/functions.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Function to add site features that will be used by the aj_billing plugin to create plans for the site
+ * Features can be of 2 types currently : 
+ *  1. yes_no_type - those that can only be enabled or disabled - eg. Domain mapping
+ *  2. count_type - those that can be enabled/disabled as well as have a count associated to them - eg. Email accounts
+ * Each feature has a name and a key. the key is a unique single word and will be used to refer the feature
+ */
+function impruw_add_feature_components($defined_feature_components){
+
+    $yes_no_features = array(
+        array('key' =>'domain_mapping' , 'name' => 'Domain Mapping' )
+    );
+
+    $count_features =  array(array('key' =>'email_account' , 'name' => 'Email Account' ),
+    array('key'=> 'site_add_ons', 'name' => 'Site Add Ons' ));
+
+    $feature_components['yes_no_type'] = $yes_no_features;
+    $feature_components['count_type'] = $count_features;
+
+    return $feature_components;
+
+}
+add_filter('add_feature_components_filter','impruw_add_feature_components',10,1);
+
+
+/**
+ * Registered feature types : domain_mapping , email_sccount , site_add_ons
+ */
+function is_feature_allowed($feature_component){
+    $site_id = get_current_blog_id();
+    $is_allowed = 0;
+
+    $user_feature = ajbilling_is_this_user_allowed($site_id , 'site', $feature_component);
+    if ($user_feature['code']==='OK') {
+    	$is_allowed = $user_feature['allowed'];
+    }
+       
+    return $is_allowed;
+}
+
+
