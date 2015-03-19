425618acfd249f5dabe068d97e41e670a664fc25
Nutan Kamat <nutan@ajency.in>
2015-03-19 18:28:10 +0530

    function to collect info for assisted setup email

--- wp-content/themes/HighendWP-child/modules/communications/new_blog/functions.php
+++ wp-content/themes/HighendWP-child/modules/communications/new_blog/functions.php
@@ -62,13 +62,56 @@ function registered_email_notification($blog_id,$user_id,$domain,$path,$site_id,
         'status' => 'linedup'
     );
 
-    /*print_r($comm_data);
-    print_r($meta_data);
-    print_r($recipients_email);*/
-    //var_dump($recipients_email_admin); exit;
 
     $aj_comm->create_communication($comm_data_admin,$meta_data,$recipients_email_admin);
     $aj_comm->create_communication($comm_data_new,$meta_data,$recipients_email_new);
 
 }
 add_action('wpmu_new_blog','registered_email_notification',10,6);
+
+function assisted_setup_contact_email($user_id, $blog_id,$assisted_setup_details){
+    global $aj_comm;
+
+    $user_info  = get_userdata($user_id);
+    $username   = $user_info->user_login;
+
+    $blog_details = get_blog_details($blog_id);
+    $site_name = $blog_details->blogname;
+
+    $reg_user_email = $user_info->user_email;
+    $reg_date       =  date("Y/m/d");
+
+    if ($assisted_setup_details['assisted_setup_contact_mode'] == 'by_email') {
+       $assisted_setup_contact_value = $user_info->user_email;
+    }
+    else if ($assisted_setup_details['assisted_setup_contact_mode'] == 'by_phone'){
+       $assisted_setup_contact_value = $assisted_setup_details['assisted_setup_contact_phone'];    
+    }
+
+    $meta_data = array(
+      'assisted_setup_contact_value'=> $assisted_setup_contact_value,
+      'assisted_setup_contact_mode'=> $assisted_setup_details['assisted_setup_contact_mode']
+    );
+
+    $comm_data_admin = array(
+        'component' => 'impruw_user',
+        'communication_type' => 'assisted_set_up_contact',
+        'user_id' => $user_id,
+        'blog_id' =>$blog_id
+    );
+
+
+    $all_users = get_users('role=impruw_manager');
+
+    foreach($all_users as $user){
+
+        $recipients_email_admin[] = array(
+            'user_id' => $user->ID,
+            'type' => 'email',
+            'value' => $user->user_email,
+            'status' => 'linedup'
+        );
+    }
+
+    $aj_comm->create_communication($comm_data_admin,$meta_data,$recipients_email_admin);
+}
