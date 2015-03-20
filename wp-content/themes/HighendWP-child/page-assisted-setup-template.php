3ecab47e7deb9c6d937de642e42fbfd8e13a4f19
Nutan Kamat <nutan@ajency.in>
2015-03-20 13:05:06 +0530

    html fix for assisted setup page

--- wp-content/themes/HighendWP-child/page-assisted-setup-template.php
+++ wp-content/themes/HighendWP-child/page-assisted-setup-template.php
@@ -17,19 +17,19 @@ get_header();
       <br><br>
       <div class="row fw-content-wrap">
          <div class="col-1">
-            <b style="text-align: center; font-weight: bolder;">1</b>
+            <b style="text-align: right; font-weight: bolder; display:block;">1</b>
          </div>
          <div class="col-11">
             Please select how and when you want to be contacted
-            <div class="row fw-content-wrap">
+            <div class="row fw-content-wrap form-group">
                <div class="col-4">
-                  <input name="assisted_setup_contact_mode" type="radio" value="by_email"  id = "assisted_setup_contact_email"parsley-required="true" parsley-required-message="<?php echo __('Please select a mode of contact.', 'impruwmain'); ?>"  />  spencer@impruw.com
+                  <input name="assisted_setup_contact_mode" type="radio" value="by_email"  id = "assisted_setup_contact_email"parsley-required="true" parsley-required-message="<?php echo __('Please select a mode of contact.', 'impruwmain'); ?>"  class="form-control" />  spencer@impruw.com
                </div>
                <div class="col-2">
-               <input name="assisted_setup_contact_mode" type="radio" id = "assisted_setup_contact_phone" value="by_phone"/> By phone
+               <input name="assisted_setup_contact_mode" type="radio" id = "assisted_setup_contact_phone" value="by_phone" class="form-control"/> By phone
                </div>
                <div class="col-4">
-                  <input name="phone_number" type="number" placeholder="Phone no" data-parsley-type="number"/>
+                  <input name="phone_number" type="tel" pattern="[0-9]{10}" placeholder="Phone no" class="form-control" />
                   <div class='p-messages phone-number'></div>
                </div>
             </div>
@@ -38,7 +38,7 @@ get_header();
       <br><br>
       <div class="row fw-content-wrap">
          <div class="col-1">
-            <b style="text-align: center; font-weight: bolder;">2</b>
+            <b style="text-align: right; font-weight: bolder; display:block;" >2</b>
          </div>
          <div class="col-11">
             We will contact you within 24 hours to discuss your new website and our payment process
@@ -47,7 +47,7 @@ get_header();
       <br><br>
       <div class="row fw-content-wrap">
          <div class="col-1">
-            <b style="text-align: center; font-weight: bolder;">3</b>
+            <b style="text-align: right; font-weight: bolder; display:block;">3</b>
          </div>
          <div class="col-11">
            After payment has been received we will build your website and transfer it to your domain in 48 hours or less 
@@ -57,12 +57,19 @@ get_header();
       <input type="hidden" name="language" value="<?php echo $_GET['language']; ?>">
       <input type="hidden" name ="site_id" value="<?php echo $_GET['site']; ?>">
       <input type="hidden" name ="user_id" value="<?php echo $_GET['user']; ?>">
-      <button class="hb-button hb-carrot no-three-d aj-imp-submit" id="btn_assisted_setup_info" name="btn_assisted_setup_info">Take me to my dashboard</button>
+
+      <div class="row fw-content-wrap">
+          <div class="col-1">&nbsp;</div>
+          <div class="col-11">
+            <button class="hb-button hb-carrot no-three-d aj-imp-submit" id="btn_assisted_setup_info" name="btn_assisted_setup_info">Take me to my dashboard</button>
+         </div>
+      </div>
+
+
    </div>
-   </button>
    </div>
    </form>
 
-</div>
+
 <?php
 get_footer();
