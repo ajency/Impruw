98c182735e3692888adac1c39d874389f3a3259a
Nutan Kamat <nutan@ajency.in>
2015-05-10 15:59:00 +0530

    code commented that sent main site contact us email via wp mail function

--- wp-content/themes/HighendWP/functions.php
+++ wp-content/themes/HighendWP/functions.php
@@ -1255,21 +1255,21 @@
 
     /* AJAX MAIL
     ================================================== */
-    add_action('wp_ajax_mail_action', 'sending_mail');
-    add_action('wp_ajax_nopriv_mail_action', 'sending_mail');
-    function sending_mail() {
-        $site     = get_site_url();
-        $subject  = __('New Message!', 'hbthemes');
-        $email    = $_POST['contact_email'];
-        $email_s  = filter_var($email, FILTER_SANITIZE_EMAIL);
-        $comments = stripslashes($_POST['contact_comments']);
-        $name     = stripslashes($_POST['contact_name']);
-        $to       = hb_options('hb_contact_settings_email');
-        $message  = "Name: $name \n\nEmail: $email \n\nMessage: $comments \n\nThis email was sent from $site";
-        $headers  = 'From: ' . $name . ' <' . $email_s . '>' . "\r\n" . 'Reply-To: ' . $email_s;
-        mail($to, $subject, $message, $headers);
-        exit();
-    }
+    // add_action('wp_ajax_mail_action', 'sending_mail');
+    // add_action('wp_ajax_nopriv_mail_action', 'sending_mail');
+    // function sending_mail() {
+    //     $site     = get_site_url();
+    //     $subject  = __('New Message!', 'hbthemes');
+    //     $email    = $_POST['contact_email'];
+    //     $email_s  = filter_var($email, FILTER_SANITIZE_EMAIL);
+    //     $comments = stripslashes($_POST['contact_comments']);
+    //     $name     = stripslashes($_POST['contact_name']);
+    //     $to       = hb_options('hb_contact_settings_email');
+    //     $message  = "Name: $name \n\nEmail: $email \n\nMessage: $comments \n\nThis email was sent from $site";
+    //     $headers  = 'From: ' . $name . ' <' . $email_s . '>' . "\r\n" . 'Reply-To: ' . $email_s;
+    //     mail($to, $subject, $message, $headers);
+    //     exit();
+    // }
 
 
     /* TIME AGO
